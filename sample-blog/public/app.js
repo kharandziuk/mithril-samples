6f38385f9b12cda87165eb4755fad13013f20954
Sergii Iushchuk <edifear@gmail.com>
2014-10-30 12:21:49 +0200

    new posts, post details, posts list refactored

--- sample-blog/public/app.js
+++ sample-blog/public/app.js
@@ -1,69 +1,101 @@
-var app = {}
+/*global m: false */
+'use strict';
 
-app.Post = function(data) {
-    this.title = m.prop(data.title);
-}
+var app = {};
 
+app.PostList = function(list) {
+  return m.prop(list || []);
+};
 
-app.PostList = Array;
-
-
-app.vm = {}
+app.vm = {};
 app.vm.init = function() {
-  posts = new app.PostList();
-  this.posts = posts;
-
-  m.request({method: 'GET', url: '/posts'}).then(function(list){
-    console.log(list)
-    list.forEach(function(el) {
-      posts.push(el);
-    });
-  });
-}
+  this.posts = new app.PostList();
+  this.posts = m.request({method: 'GET', url: '/posts'});
+};
 
 app.controller = function() {
-  app.vm.init()
-  this.click = function() {
-    m.route('posts/add');
-  }
-}
+  app.vm.init();
+};
 
 app.view = function(ctrl) {
   return m('body', [
       m('h1', 'My blog'),
-      m('ul', app.vm.posts.map(function(el){
-          return m('li', el.title);
+      m('ul',
+        app.vm.posts().map(function(el){
+          return m('li', [
+            m('a[href="/posts/'+el.id+'"]', {config: m.route}, el.title)
+          ]);
         })
       ),
-      m(
-        'button', { onclick: ctrl.click }, 'add new post'
-      )
-  ])
+      m('a[href="/posts/add/"]', {config: m.route}, 'add new post')
+  ]);
 };
 
 var add = {};
 
-add.controller = function() {
-
+add.newPost = function(data) {
+  data = data || {};
+  this.title = data.title || '';
+  this.content = data.content || '';
 };
+
+add.controller = function() {
+  var post = new add.newPost();
+  this.post = post;
+  this.create = function(e) {
+    e.preventDefault();
+    m.request({
+      method: 'POST',
+      url: '/posts',
+      data: post
+    }).then(function() {m.route('/');});
+  };
+};
+
+// data binding helper function
+// http://lhorie.github.io/mithril-blog/asymmetrical-data-bindings.html
+function formBinds(data) {
+  return function(e) {data[e.target.name] = e.target.value;};
+}
 
 add.view = function(ctrl) {
   return m('body', [
-            m('a[href=""]>', {config: m.route}, 'All Posts'),
-            m('br'),
-            m('input[type="text",placeholder="post title"]'),
-            m('textarea'),
-            m('br'),
-            m('button', 'Post'),
-  ])
-}
+    m('form', {
+      onsubmit: ctrl.create,
+      onchange: formBinds(ctrl.post)
+    }, [
+      m('a[href="/"]', {config: m.route}, 'All Posts'),
+      m('br'),
+      m('input[type=text][name=title][placeholder="post title"][required]'),
+      m('br'),
+      m('textarea[name=content][placeholder="post content"][required]'),
+      m('br'),
+      m('input[type=submit][value=Post]')
+    ])
+  ]);
+};
 
+var details = {};
 
+details.controller = function() {
+  this.post = {};
+  this.post.id = m.route.param('postId');
+};
 
+details.view = function(ctrl) {
+  return m('body', [
+    m('a[href="/"]', {config: m.route}, 'All posts'),
+    m('h1', 'post id: '+ctrl.post.id)
+  ]);
+};
+
+//setup routes to start w/ the `#` symbol
+m.route.mode = 'hash';
+
+//define a route
 m.route(document, '/', {
-    '' : app,
-    'posts/add': add,
-    //'posts/new': newPost,
-    //'posts/:id': singlePost
-})
-     
+    '/' : app,
+    '/posts/add': add,
+    '/posts/:postId': details
+});
+
--- sample-blog/server.js
+++ sample-blog/server.js
@@ -3,44 +3,47 @@
 
 var express = require('express');
 var path    = require('path');
-var Bourne  = require("bourne");
+var Bourne  = require('bourne');
 
 var app      = express();
-var posts    = new Bourne("simpleBlogPosts.json");
-var comments = new Bourne("simpleBlogComments.json");
+var posts    = new Bourne('simpleBlogPosts.json');
+var comments = new Bourne('simpleBlogComments.json');
 
-app.configure(function () {
-    app.use(express.json());
-    app.use(express.static(path.join(__dirname, 'public')));
-});
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/posts", function (req, res) {
+app.get('/posts', function (req, res) {
     posts.find(function (err, results) {
         res.json(results);
     });
 });
-app.post("/posts", function (req, res) {
+
+app.get('/posts/:id', function (req, res) {
+    posts.find(function (err, results) {
+        res.json(results);
+    });
+});
+
+app.post('/posts', function (req, res) {
     posts.insert(req.body, function (err, result) {
         res.json(result);
     });
 });
 
-app.get("/posts/:id/comments", function (req, res) {
+app.get('/posts/:id/comments', function (req, res) {
     comments.find({ postId: parseInt(req.params.id, 10) }, function (err, results) {
         res.json(results);
     });
 });
 
-app.post("/posts/:id/comments", function (req, res) {
+app.post('/posts/:id/comments', function (req, res) {
     comments.insert(req.body, function (err, result) {
         res.json(result);
     });
 });
 
 app.get('/*', function (req, res) {
-    posts.find(function (err, results) {
-        res.render("index.ejs", { posts: JSON.stringify(results) });
-    });
+    res.render('index.ejs');
 });
 
 app.listen(3000);
